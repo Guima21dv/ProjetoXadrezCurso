99483f54569d952ddcb4defee1f789893082f259
Guilherme Guimarães <guima_dev@hotmail.com>
2018-12-21 00:20:54 -0200

    Foi iniciado a implementação do tabuleiro e outras classes necessárias

---
+++ sec12_projeto_xadrez/Tabuleiro/Cor.cs
@@ -0,0 +1,14 @@
+﻿
+namespace sec12_projeto_xadrez.Tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Roxa,
+        Vermelha,
+        Verde
+    }
+}
---
+++ sec12_projeto_xadrez/Tabuleiro/Peca.cs
@@ -0,0 +1,27 @@
+﻿using Tabuleiro;
+
+namespace sec12_projeto_xadrez.Tabuleiro
+{
+    class Peca
+    {
+
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QuantidadeDeMovimentos { get; protected set; }
+        public Tabuleiro Tabuleiro { get; set; }
+
+
+        public Peca()
+        {
+
+        }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QuantidadeDeMovimentos = 0;
+            Tabuleiro = tabuleiro;
+        }
+    }
+}
